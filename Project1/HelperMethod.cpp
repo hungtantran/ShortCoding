eaff22312999fa0e7df735073c617b690c5bf41d
hungtantran <hungtantran@live.com>
2015-07-27 00:18:46 -0700

    Add print method for binary tree

--- Project1/HelperMethod.cpp
+++ Project1/HelperMethod.cpp
@@ -1,4 +1,7 @@
 #include <time.h>
+#include <queue>
+#include <iostream>
+
 #include "HelperMethod.h"
 
 using namespace std;
@@ -11,7 +14,7 @@ void HelperMethod::printArray(const vector<int>& arr)
 	}
 
 	printf("\n");
-}
+};
 
 void HelperMethod::generateArray(vector<int>& arr, int size, int min, int max)
 {
@@ -20,4 +23,85 @@ void HelperMethod::generateArray(vector<int>& arr, int size, int min, int max)
 		int val = rand() % (max - min + 1) + min;
 		arr.emplace_back(val);
 	}
-}
+};
+
+Node_BinaryTree* HelperMethod::generateBinarySearchTree(bool balance, int depth)
+{
+    if (depth == 0)
+    {
+        return NULL;
+    }
+
+    Node_BinaryTree* root = new Node_BinaryTree();
+
+    // TODO complete it
+
+    return root;
+};
+
+Node_BinaryTree* HelperMethod::generateBinaryTree(bool balance, int depth)
+{
+    if (depth == 0)
+    {
+        return NULL;
+    }
+
+    Node_BinaryTree* root = new Node_BinaryTree();
+    root->value = rand() % 100000;
+
+    int randLeft = rand() % depth;
+    if (balance)
+    {
+        randLeft = 1;
+    }
+    Node_BinaryTree* left = HelperMethod::generateBinaryTree(balance, depth - randLeft);
+
+    int randRight = rand() % depth;
+    if (balance)
+    {
+        randRight = 1;
+    }
+    Node_BinaryTree* right = HelperMethod::generateBinaryTree(balance, depth - randRight);
+
+    root->leftNode = left;
+    root->rightNode = right;
+
+    return root;
+};
+
+void Node_BinaryTree::Print()
+{
+    this->level = 1;
+
+    std::queue<Node_BinaryTree*> queue;
+    queue.emplace(this);
+
+    int lastLevel = 0;
+    while (!queue.empty())
+    {
+        Node_BinaryTree* cur = queue.front();
+        queue.pop();
+
+        if (cur->level > lastLevel)
+        {
+            std::cout << std::endl;
+            lastLevel = cur->level;
+        }
+
+        std::cout << cur->value << "(" << cur->level << ")" << "\t";
+
+        Node_BinaryTree* left = cur->leftNode;
+        if (left != NULL)
+        {
+            left->level = cur->level + 1;
+            queue.push(left);
+        }
+
+        Node_BinaryTree* right = cur->rightNode;
+        if (right != NULL)
+        {
+            right->level = cur->level + 1;
+            queue.push(right);
+        }
+    }
+};
--- Project1/HelperMethod.h
+++ Project1/HelperMethod.h
@@ -1,9 +1,35 @@
 #include <vector>
 
+struct Node_BinaryTree
+{
+    int value;
+    Node_BinaryTree* leftNode;
+    Node_BinaryTree* rightNode;
+    int level;
+
+    Node_BinaryTree() :
+        value(INT_MIN),
+        leftNode(NULL),
+        rightNode(NULL)
+    {
+    }
+
+    Node_BinaryTree(int value, Node_BinaryTree* left, Node_BinaryTree* right) :
+        value(value), leftNode(left), rightNode(right)
+    {
+    }
+
+    void Print();
+};
+
 class HelperMethod
 {
 public:
 	static void printArray(const std::vector<int>& arr);
 
 	static void generateArray(std::vector<int>& arr, int size, int min, int max);
+
+    static Node_BinaryTree* generateBinarySearchTree(bool balance = false, int depth = 10);
+
+    static Node_BinaryTree* generateBinaryTree(bool balance = false, int depth = 10);
 };
