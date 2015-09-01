7710b702db027fc278b75b557eece9f1d6311a56
hungtantran <hungtantran@live.com>
2015-09-01 01:18:53 -0700

    Add more misc

--- Project1/Misc.cpp
+++ Project1/Misc.cpp
@@ -6,6 +6,7 @@
 #include <sstream>
 #include <string>
 #include <set>
+#include "HelperMethod.h"
 
 using namespace std;
 
@@ -156,6 +157,196 @@ int snake_ladder(const vector<int>& moves)
 	return snake_ladder(moves, 0, steps, cost);
 }
 
+void generate_binary_representation_ones_greater_than_zeroes(int length, vector<int>& vals, int numOnes, int numZeroes)
+{
+    if (length == 0)
+    {
+        for (int i = 0; i < vals.size(); ++i)
+        {
+            cout << vals[i];
+        }
+        cout << endl;
+        return;
+    }
+
+    if (numOnes > numZeroes)
+    {
+        vals.emplace_back(0);
+        generate_binary_representation_ones_greater_than_zeroes(length - 1, vals, numOnes, numZeroes + 1);
+        vals.pop_back();
+    }
+
+    vals.emplace_back(1);
+    generate_binary_representation_ones_greater_than_zeroes(length - 1, vals, numOnes + 1, numZeroes);
+    vals.pop_back();
+}
+
+void generate_binary_representation_ones_greater_than_zeroes(int length)
+{
+    vector<int> vals;
+    generate_binary_representation_ones_greater_than_zeroes(length, vals, 0, 0);
+}
+
+string minimum_string_rotation(const string& org)
+{
+    string min = org;
+    string doubleOrg = org + org;
+
+    for (int i = 1; i < org.size(); ++i)
+    {
+        string cur = doubleOrg.substr(i, org.length());
+        if (cur < min)
+        {
+            min = cur;
+        }
+    }
+
+    return min;
+}
+
+vector<int> construct_array_pair_sum_array(const std::vector<int> input, int length)
+{
+    vector<int> results;
+    results.resize(length);
+
+    int sum = 0;
+    for (int i = 0; i < input.size(); ++i)
+    {
+        sum += input[i];
+    }
+    sum = sum / (length - 1);
+
+    int sumSoFar = 0;
+    int index = 0;
+    for (int i = 0; i < length - 2; ++i)
+    {
+        int curSum = sumSoFar;
+        for (int j = 0; j < (length - i - 1); ++j)
+        {
+            curSum += input[index];
+            ++index;
+        }
+
+        int nextEntry = (curSum - sum) / (length - i - 2);
+        sumSoFar += nextEntry;
+        results[i] = nextEntry;
+    }
+
+    results[length - 2] = input[length - 3] - results[0];
+    results[length - 1] = input[length - 2] - results[0];
+
+    return results;
+}
+
+bool lessOrEqual(char op1, char op2)
+{
+    if ((op1 == '+' || op1 == '-') && (op2 == '+' || op2 == '-'))
+    {
+        return true;
+    }
+
+    if ((op1 == '*' || op1 == '/') && (op2 == '*' || op2 == '/'))
+    {
+        return true;
+    }
+
+    if ((op1 == '+' || op1 == '-') && (op2 == '*' || op2 == '/'))
+    {
+        return true;
+    }
+
+    return false;
+}
+
+int evaluate_simple_expression(const string& exp)
+{
+    if (exp.length() % 2 == 0)
+    {
+        return -1;
+    }
+
+    stringstream posfix;
+    stack<char> ops;
+
+    for (int i = 0; i < exp.length(); ++i)
+    {
+        if (exp[i] == ' ')
+        {
+            continue;
+        }
+
+        if (i % 2 == 0)
+        {
+            if (exp[i] < '0' || exp[i] > '9')
+            {
+                return -1;
+            }
+
+            posfix << exp[i];
+        }
+        else
+        {
+            if (exp[i] != '+' && exp[i] != '-' && exp[i] != '*' && exp[i] != '/')
+            {
+                return -1;
+            }
+
+            while (!ops.empty() && lessOrEqual(exp[i], ops.top()))
+            {
+                posfix << ops.top();
+                ops.pop();
+            }
+
+            ops.push(exp[i]);
+        }
+    }
+
+    while (!ops.empty())
+    {
+        posfix << ops.top();
+        ops.pop();
+    }
+
+    string postfixStr = posfix.str();
+    stack<int> vals;
+    for (int i = 0; i < postfixStr.length(); ++i)
+    {
+        if (postfixStr[i] >= '0' && postfixStr[i] <= '9')
+        {
+            vals.push(postfixStr[i] - '0');
+        }
+        else
+        {
+            int val1 = vals.top();
+            vals.pop();
+            int val2 = vals.top();
+            vals.pop();
+
+            int newVal = 0;
+            if (postfixStr[i] == '+')
+            {
+                newVal = val1 + val2;
+            }
+            else if (postfixStr[i] == '-')
+            {
+                newVal = val2 - val1;
+            }
+            else if (postfixStr[i] == '/')
+            {
+                newVal = val2 / val1;
+            }
+            else if (postfixStr[i] == '*')
+            {
+                newVal = val2 * val1;
+            }
+
+            vals.push(newVal);
+        }
+    }
+
+    return vals.top();
+}
+
 // http://www.geeksforgeeks.org/find-index-0-replaced-1-get-longest-continuous-sequence-1s-binary-array/
 // int main()
 int longest_1_sequence()
@@ -251,3 +442,77 @@ int snake_ladder()
 
 	return 0;
 }
+
+// http://geeksquiz.com/generate-binary-permutations-1s-0s-every-point-permutations/
+// int main()
+int generate_binary_representation_ones_greater_than_zeroes()
+{
+    generate_binary_representation_ones_greater_than_zeroes(1);
+    cout << endl;
+
+    generate_binary_representation_ones_greater_than_zeroes(2);
+    cout << endl;
+
+    generate_binary_representation_ones_greater_than_zeroes(3);
+    cout << endl;
+
+    generate_binary_representation_ones_greater_than_zeroes(4);
+    cout << endl;
+
+    generate_binary_representation_ones_greater_than_zeroes(5);
+    cout << endl;
+
+    int test;
+    cin >> test;
+
+    return 0;
+}
+
+// http://geeksquiz.com/lexicographically-minimum-string-rotation/
+// int main()
+int minimum_string_rotation()
+{
+    cout << minimum_string_rotation("GEEKSQUIZ") << endl;
+    cout << minimum_string_rotation("GFG") << endl;
+    cout << minimum_string_rotation("GEEKSFORGEEKS") << endl;
+
+    int test;
+    cin >> test;
+
+    return 0;
+}
+
+// http://geeksquiz.com/construct-array-pair-sum-array/
+// int main()
+int construct_array_pair_sum_array()
+{
+    vector<int> input1 = { 14, 9, 10, 11, 12, 7 };
+    vector<int> output1 = construct_array_pair_sum_array(input1, 4);
+    HelperMethod::printArray(output1);
+    cout << endl;
+
+    vector<int> input2 = { 15, 13, 11, 10, 12, 10, 9, 8, 7, 5 };
+    vector<int> output2 = construct_array_pair_sum_array(input2, 5);
+    HelperMethod::printArray(output2);
+    cout << endl;
+
+    int test;
+    cin >> test;
+
+    return 0;
+}
+
+// http://geeksquiz.com/program-evaluate-simple-expressions/
+// int main()
+int evaluate_simple_expression()
+{
+    cout << evaluate_simple_expression("1+2*3") << endl;
+    cout << evaluate_simple_expression("4-2+6*3") << endl;
+    cout << evaluate_simple_expression("1++2") << endl;
+
+    int test;
+    cin >> test;
+
+    return 0;
+}
+
